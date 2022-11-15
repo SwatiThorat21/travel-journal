fed6dace7db987b7ca187ba1778240e9a6bdd45a
Swati <swatic019@gmail.com>
2022-11-15 08:41:45 +0530

    Card component updated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from "react"
 import './App.css';
 import data from './data';
 import Navbar from './components/Navbar'
+import Card from './components/Cards'
 
 function App() {
   console.log(data)
+  let cards = data.map(item =>{
+return(
+  <Card 
+  key = {item.id}
+  imageUrl = {item.imageUrl}
+  location = {item.location}
+  googleMapsUrl = {item.googleMapsUrl}
+  title = {item.title}
+  startDate = {item.startDate}
+  endDate = {item.endDate}
+  description = {item.description}
+/>
+)
+
+  })
   return (
     <div className="container">
     <Navbar />
+    {cards}
+    
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,24 @@
-import React from "react"
+import React from "react";
 
-export default function Cards(){
-    return(
-        <div className="cards">
-        <img src="https://source.unsplash.com/WLxQvbMyfas" alt="location image" className="card-img"> </img>
-        <div className="google">
-            <img src="../../location-img.png">Japan</img>
-            <p>View on google maps</p>
-        </div>
-
-        </div>
-    )
+export default function Card(props) {
+    console.log(props)
+  return (
+    <div className="cards">
+      <img
+        src= {props.imageUrl}
+        alt="location_image"
+        className="card-img"
+      />
+      <div className="google">
+        <img src="../../location-img.png" alt="logo_img"/>
+          {props.location}
+        <a href= {props.googleMapsUrl}>View on google maps</a>
+      </div>
+      <h2>{props.title}</h2>
+      <h3>{props.startDate} - {props.endDate}</h3>
+      <p>
+        {props.description}
+      </p>
+    </div>
+  );
 }
